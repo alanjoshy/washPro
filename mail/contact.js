a18de7cdd7a94486ec58118d3aee580a8105a16b
Alan Joshy <97900506+alanjoshy@users.noreply.github.com>
2025-05-04 23:06:22 +0530

    Add files via upload

---
+++ mail/contact.js
@@ -0,0 +1,62 @@
+$(function () {
+
+    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault();
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var subject = $("input#subject").val();
+            var message = $("textarea#message").val();
+
+            $this = $("#sendMessageButton");
+            $this.prop("disabled", true);
+
+            // EmailJS send function
+            emailjs.send("service_2u3t3s7", "template_j404s6k", {
+                name: name,
+                email: email,
+                subject: subject,
+                message: message
+            })
+                .then(function (response) {
+                    // Success Message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+                    $('#contactForm').trigger("reset");
+                }, function (error) {
+                    // Error Message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append('</div>');
+                    $('#contactForm').trigger("reset");
+                })
+                .finally(function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false);
+                    }, 1000);
+                });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+$('#name').focus(function () {
+    $('#success').html('');
+});
